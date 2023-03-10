e5bfa862fd79d6fc3b46bfc148171f8390796b85
HUZOGL <huzogl@coloplast.com>
2023-03-10 16:13:08 +0100

    Employees model changed

--- Models/Employees.cs
+++ Models/Employees.cs
@@ -14,6 +14,7 @@
         public string? Address { get; set; }
         public int JobId { get; set; }
         public int StatusId { get; set; }
+        public bool IsDeleted { get; set; }
 
         public virtual Statuses? Statuses { get; set; }
         public virtual ICollection<AllowancesOfEmployees>? AllowancesOfEmployees { get; set; }
